2413b249c23f33b9a3795fb7f6c00d2fd77d5043
AbdulMalik Sharif <abdumaliksharipov@gmail.com>
2022-11-27 09:50:19 +0500

    feedback form logic

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+
+import { useState, useEffect,useCallback } from 'react';
 import { useTelegram } from '../../hooks/useTelegram';
 import './Form.css';
 
@@ -7,6 +8,22 @@ const Form = () => {
   const [street, setStreet] = useState('');
   const [subject, setSubject] = useState('physical');
   const { tg } = useTelegram();
+
+  const onSendData=useCallback(()=>{
+    const data={
+      country,street,subject
+    }
+
+    tg.sendData(JSON.stringify(data))
+  },[country,street,subject])
+
+  useEffect(()=>{
+    tg.onEvent('mainButtonClicked', onSendData)
+
+    return ()=>{
+      tg.offEvent('mainButtonClicked', onSendData)
+    }
+  },[onSendData])
 
   useEffect(() => {
     tg.MainButton.setParams({
